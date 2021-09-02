7767a32aaca3d5761c8176d9c87f6adfb768059d
rsavage <rsavage3794@gmail.com>
2021-09-01 21:37:21 -0400

    changed name of app

---
+++ rotten_potatoes/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.db.models import constraints
+
+# Create your models here.
+class Show(models.Model):
+    title: models.CharField(max_length=100)
+    notable_actors: models.TextField()
+    network: models.CharField(max_length=100)
+    genre: models.CharField(max_length=100)
+    summary: models.TextField()
+    rating: models.PositiveIntegerField(null=True)
+    photo_url: models.TextField()
+    
+class Meta:
+    constraints = [
+        models.CheckConstraint(
+            name="%(app_label)s_%(class)s_rating_range",
+            check=models.Q(page_count__range=(0, 10)),
+        ),
+    ]
---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE rotten_potatoes;
+CREATE USER couch WITH PASSWORD 'password123';
+GRANT ALL PRIVILEGES ON DATABASE rotten_potatoes to couch;
