81fbd727e1a77092e922fd8446279f0c92c9fbdc
rsavage <rsavage3794@gmail.com>
2021-09-12 16:17:49 -0400

    adding picture

--- Frontend/frontend_potatoes/src/components/HomePage/HomePage.js
+++ Frontend/frontend_potatoes/src/components/HomePage/HomePage.js
@@ -4,6 +4,7 @@ const HomePage = () => {
     return (
         <div>
             <h1>Rotten Potatoes</h1>
+            <img src={require ('../../rotten_potato.svg')} alt="Rotten Potatoes Logo" />
         </div>
     )
 }
